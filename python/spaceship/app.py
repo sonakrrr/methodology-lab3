420c93ac818b5991b2a5d567fa9736c853af639d
Sofiia Krytska <krytskasofia@gmail.com>
2024-05-28 14:53:46 +0300

    create new endpoint

--- python/spaceship/app.py
+++ python/spaceship/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
+from typing import List, Dict
+import numpy as np
 
 from spaceship.config import Settings
 from spaceship.routers import api, health
@@ -24,5 +26,16 @@ def make_app(settings: Settings) -> FastAPI:
     @app.get('/', include_in_schema=False, response_class=FileResponse)
     async def root() -> str:
         return 'build/index.html'
+
+    @app.get('/matrix/', response_model=Dict[str, List[List[float]]])
+    async def multiply_matrices_endpoint(rows1: int = 10, cols1: int = 10, rows2: int = 10, cols2: int = 10):
+        matrix1 = np.random.rand(rows1, cols1)
+        matrix2 = np.random.rand(rows2, cols2)
+        result = np.dot(matrix1, matrix2)
+        return {
+            'matrix_a': matrix1.tolist(),
+            'matrix_b': matrix2.tolist(),
+            'result': result.tolist()
+        }
 
     return app
